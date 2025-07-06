cd5001cc3483e90840e6d87577de70d199c1e484
Utsav Saxena <utsavmra2004@gmail.com>
2025-07-06 10:22:44 +0530

    Add WEEK 3 HandsOn

---
+++ WEEK_3__Entity_Framewrork_Core/RetailInventory/AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RetailInventory.Models;
+using Microsoft.Extensions.Configuration;
+
+namespace RetailInventory
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext() { }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS;Database=RetailDb;Trusted_Connection=True;TrustServerCertificate=True;");
+        }
+    }
+
+
+}
---
+++ WEEK_3__Entity_Framewrork_Core/RetailInventory/Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+ ============================
+    Author - Utsav Saxena
+    Date - 05-07-25
+ ============================
+*/
+
+using Microsoft.EntityFrameworkCore;
+using RetailInventory;
+using RetailInventory.Models;
+
+using var context = new AppDbContext();
+
+var electronics = new Category { Name = "Electronics" };
+var groceries = new Category { Name = "Groceries" };
+var author = new Category { Name = "Author" };
+await context.Categories.AddRangeAsync(electronics, groceries, author);
+var product1 = new Product { Name = "Laptop", Price = 75000, Category = electronics };
+var product2 = new Product { Name = "Rice Bag", Price = 1200, Category = groceries };
+var product3 = new Product { Name = "Utsav Saxena", Price = 6361856, Category = author };
+await context.Products.AddRangeAsync(product1, product2, product3);
+await context.SaveChangesAsync();
+
+
+Console.WriteLine("Name - Utsav Saxena, Superset ID - 6361856 \n");
+var products = await context.Products.ToListAsync();
+foreach (var p in products)
+    Console.WriteLine($"{p.Name} - ₹{p.Price}");
+
+var product = await context.Products.FindAsync(1);
+Console.WriteLine($"Found: {product?.Name}");
+
+var expensive = await context.Products.FirstOrDefaultAsync(p => p.Price > 50000);
+Console.WriteLine($"Expensive: {expensive?.Name}");
